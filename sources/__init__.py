5d296011ce97a71dba3a2fb202d9864c6bfcaa67
Gautam Mittal <gautam@mittal.net>
2020-03-26 22:34:28 -0700

    Add Chinese registry back.

--- sources/__init__.py
+++ sources/__init__.py
@@ -9,7 +9,7 @@ TERMS = utils.get_query_terms()
 def get_records():
     data = {}
     for query in TERMS:
-        # data.update(chictr.find(query))
+        data.update(chictr.find(query))
         data.update(clinicaltrialsgov.find(query))
         data.update(eu.find(query))
         data.update(isrctn.find(query))
