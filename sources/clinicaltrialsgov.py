a9d0cd6a3417c25b3e1677c032cadc0a6495cef1
Noah Saso <noahsaso@gmail.com>
2020-03-27 14:39:24 -0700

    Add summary fields to clinicaltrials.gov output (#23)
    * add summary fields to clinicaltrials.gov output (proof of concept)

--- sources/clinicaltrialsgov.py
+++ sources/clinicaltrialsgov.py
@@ -3,9 +3,29 @@ import os
 import utils
 from dateutil import parser
 from datetime import timezone
+from bs4 import BeautifulSoup
 
 FILENAME = "clinicaltrialsgov.json"
 POSTED_WITHIN_DAYS = 200  # posted within the last X days
+
+
+def summary_to_dict(summary):
+    fields = {}
+    soup = BeautifulSoup(summary, "html.parser")
+    for b in soup.findAll("b"):
+        key = b.get_text()
+        # remove colon
+        value = b.next_sibling and b.next_sibling[1:].replace("\xa0", " ").strip()
+        # if 'recruit' in key.lower() and not value:
+        #     value = key
+        #     key = 'Stage'
+        fields[key] = value
+    # last one has value of None, is stage
+    if not fields[key]:
+        fields["Stage"] = key
+        del fields[key]
+    return fields
+
 
 def find(term):
     data = {}
@@ -37,8 +57,7 @@ def find(term):
         added_ids.add(identifier)
 
         title = entry["title"]
-        url = entry["link"].replace(f'cond={term}&', '')
-        summary = entry["summary"]
+        url = entry["link"].replace(f"cond={term}&", "")
 
         # published = entry['published_parsed']
         # iso = time.strftime('%Y-%m-%dT%H:%M:%S%z', published)
@@ -47,11 +66,18 @@ def find(term):
         )  # '%a, %d %b %Y %H:%M:%S %Z'
         iso = date.astimezone(timezone.utc).isoformat()
 
+        # fields = summary_to_dict(entry["summary"])
+
         entry_dict = {
             "title": title,
             "url": url,
             "timestamp": iso,
+            # "fields": fields,
         }
+
+        for key, value in summary_to_dict(entry["summary"]).items():
+            entry_dict[key] = value
+
         data[url] = entry_dict
 
     print(f"Fetched {len(data)} results")
