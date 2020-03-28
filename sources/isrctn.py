485c5d880668a93ab733c116f4afa7336a8460ee
Ethan Houston <ethan.houston@gmail.com>
2020-03-27 22:21:13 -0700

    Add all data from ISRCTN page

--- sources/isrctn.py
+++ sources/isrctn.py
@@ -11,7 +11,10 @@ PAGINATE_QUERY = "&page={page_num}&searchType=basic-search"
 
 def to_iso8601(date):
     comps = date.split("/")
-    return f"{comps[2]}-{comps[1]}-{comps[0]}"
+    if len(comps) == 3:
+        return f"{comps[2]}-{comps[1]}-{comps[0]}"
+    else:
+        return None
 
 
 def parse_plain_english_summary(summary):
@@ -23,27 +26,28 @@ def parse_plain_english_summary(summary):
     p6 = "When is the study starting and how long is it expected to run for?"
     p7 = "Who is funding the study?"
     p8 = "Who is the main contact?"
-    background_study_aims = summary.split(p1)[1].split(p2)[0]
-    who_can_participate = summary.split(p2)[1].split(p3)[0]
-    study_involves = summary.split(p3)[1].split(p4)[0]
-    benefits_risks = summary.split(p4)[1].split(p5)[0]
-    where_run_from = summary.split(p5)[1].split(p6)[0]
-    when_start_how_long = summary.split(p6)[1].split(p7)[0]
-    who_funding = summary.split(p7)[1].split(p8)[0]
-    main_contact = summary.split(p8)[1]
 
-    summary_data = {
-        "background_study_aims": background_study_aims,
-        "who_can_participate": who_can_participate,
-        "study_involves": study_involves,
-        "benefits_risks": benefits_risks,
-        "where_run_from": where_run_from,
-        "when_start_how_long": when_start_how_long,
-        "who_funding": who_funding,
-        "main_contact": main_contact,
-    }
+    summary_data = {}
+    try:
+        background_study_aims = summary.split(p1)[1].split(p2)[0]
+        summary_data["background_study_aims"] = background_study_aims
+        who_can_participate = summary.split(p2)[1].split(p3)[0]
+        summary_data["who_can_participate"] = who_can_participate
+        study_involves = summary.split(p3)[1].split(p4)[0]
+        summary_data["study_involves"] = study_involves
+        benefits_risks = summary.split(p4)[1].split(p5)[0]
+        summary_data["benefits_risks"] = benefits_risks
+        where_run_from = summary.split(p5)[1].split(p6)[0]
+        summary_data["where_run_from"] = where_run_from
+        when_start_how_long = summary.split(p6)[1].split(p7)[0]
+        summary_data["when_start_how_long"] = when_start_how_long
+        who_funding = summary.split(p7)[1].split(p8)[0]
+        summary_data["who_funding"] = who_funding
+        main_contact = summary.split(p8)[1]
+        summary_data["main_contact"]
+    except Exception as e:
+        return summary_data
 
-    # pprint(summary_data)
     return summary_data
 
 
@@ -95,15 +99,205 @@ def find(query):
                                 summary_data = parse_plain_english_summary(
                                     plain_english_summary
                                 )
+
                                 cleaned_ps = [p.text.strip().rstrip() for p in ps[1:]]
-                                # TODO: Get all additional data
-                                # example url: https://www.isrctn.com/ISRCTN51287266?q=covid&filters=&sort=&offset=4&totalResults=4&page=1&pageSize=10&searchType=basic-search
+
+                                primary_contact = {
+                                    "type": cleaned_ps[1],
+                                    "name": cleaned_ps[2],
+                                    "orcid_id": cleaned_ps[3],
+                                    "contact_details": cleaned_ps[4],
+                                }
+                                info_section_titles = soup.findAll(
+                                    "h3", {"class": "Info_section_title"}
+                                )
+
+                                num_additional_contacts = 0
+                                for title in info_section_titles:
+                                    if "Additional contact" in title.text:
+                                        num_additional_contacts += 1
+
+                                additional_contacts = []
+
+                                current_index = 5
+
+                                for i in range(num_additional_contacts):
+                                    contact_type = cleaned_ps[current_index]
+                                    current_index += 1
+                                    name = cleaned_ps[current_index]
+                                    current_index += 1
+                                    orcid_id = cleaned_ps[current_index]
+                                    current_index += 1
+                                    contact_details = cleaned_ps[current_index]
+                                    current_index += 1
+                                    additional_contact = {
+                                        "type": contact_type,
+                                        "name": name,
+                                        "orcid_id": orcid_id,
+                                        "contact_details": contact_details,
+                                    }
+                                    additional_contacts.append(additional_contact)
+
+                                # numbers
+                                eudract_number = cleaned_ps[current_index]
+                                current_index += 1
+                                clinical_trials_gov_number = cleaned_ps[current_index]
+                                current_index += 1
+                                protocol_serial_number = cleaned_ps[current_index]
+                                current_index += 1
+
+                                # study information
+                                scientific_title = cleaned_ps[current_index]
+                                current_index += 1
+                                acronym = cleaned_ps[current_index]
+                                current_index += 1
+                                study_hypothesis = cleaned_ps[current_index]
+                                current_index += 1
+                                ethics_approval = cleaned_ps[current_index]
+                                current_index += 1
+                                study_design = cleaned_ps[current_index]
+                                current_index += 1
+                                primary_study_design = cleaned_ps[current_index]
+                                current_index += 1
+                                secondary_study_design = cleaned_ps[current_index]
+                                current_index += 1
+                                trial_setting = cleaned_ps[current_index]
+                                current_index += 1
+                                trial_type = cleaned_ps[current_index]
+                                current_index += 1
+                                patient_information_sheet = cleaned_ps[current_index]
+                                current_index += 1
+                                condition = cleaned_ps[current_index]
+                                current_index += 1
+                                intervention = cleaned_ps[current_index]
+                                current_index += 1
+                                intervention_type = cleaned_ps[current_index]
+                                current_index += 1
+                                phase = cleaned_ps[current_index]
+                                current_index += 1
+                                drug_names = cleaned_ps[current_index]
+                                current_index += 1
+                                primary_outcome_measure = cleaned_ps[current_index]
+                                current_index += 1
+                                secondary_outcome_measure = cleaned_ps[current_index]
+                                current_index += 1
+                                overall_trial_start_date = to_iso8601(
+                                    cleaned_ps[current_index]
+                                )
+                                current_index += 1
+                                overall_trial_end_date = to_iso8601(
+                                    cleaned_ps[current_index]
+                                )
+                                current_index += 1
+                                reason_abandoned = cleaned_ps[current_index]
+                                if reason_abandoned == "":
+                                    reason_abandoned = None
+                                current_index += 1
+
+                                # eligibility
+                                participant_inclusion_criteria = cleaned_ps[
+                                    current_index
+                                ]
+                                current_index += 1
+                                participant_type = cleaned_ps[current_index]
+                                current_index += 1
+                                age_group = cleaned_ps[current_index]
+                                current_index += 1
+                                gender = cleaned_ps[current_index]
+                                current_index += 1
+                                target_num_participants = cleaned_ps[current_index]
+                                current_index += 1
+
+                                has_total_final_enrolment = False
+                                # at least one has Total final enrolment
+                                # ignore this because it is not common
+                                for title in info_section_titles:
+                                    if "Total final enrolment" in title.text:
+                                        current_index += 1
+
+                                participant_exclusion_criteria = cleaned_ps[
+                                    current_index
+                                ]
+                                current_index += 1
+                                recruitment_start_date = to_iso8601(
+                                    cleaned_ps[current_index]
+                                )
+                                current_index += 1
+                                recruitment_end_date = to_iso8601(
+                                    cleaned_ps[current_index]
+                                )
+                                current_index += 1
+
+                                # locations
+                                countries_of_recruitment = cleaned_ps[current_index]
+                                current_index += 1
+
+                                num_trial_participating_centers = 0
+                                for title in info_section_titles:
+                                    if "Trial participating centre" in title.text:
+                                        num_trial_participating_centers += 1
+
+                                trial_participation_centers = []
+
+                                for i in range(num_trial_participating_centers):
+                                    trial_participation_center = {
+                                        "info": cleaned_ps[current_index]
+                                        .strip()
+                                        .rstrip()
+                                    }
+                                    current_index += 1
+                                    trial_participation_centers.append(
+                                        trial_participation_center
+                                    )
+
+                                # sponsor information
+                                organization = cleaned_ps[current_index]
+                                current_index += 1
+                                sponsor_details = cleaned_ps[current_index]
+                                current_index += 1
+                                sponsor_type = cleaned_ps[current_index]
+                                current_index += 1
+                                sponsor_website = cleaned_ps[current_index]
+                                current_index += 1
+
+                                # funders
+                                funder_type = cleaned_ps[current_index]
+                                current_index += 1
+                                funder_name = cleaned_ps[current_index]
+                                current_index += 1
+                                alternative_name = cleaned_ps[current_index]
+                                current_index += 1
+                                funding_body_type = cleaned_ps[current_index]
+                                current_index += 1
+                                funding_body_subtype = cleaned_ps[current_index]
+                                current_index += 1
+                                location = cleaned_ps[current_index]
+                                current_index += 1
+
+                                # results and publications
+                                publication_dissemination_plan = cleaned_ps[
+                                    current_index
+                                ]
+                                current_index += 1
+                                intention_to_public_date = cleaned_ps[current_index]
+                                intention_to_public_date = to_iso8601(
+                                        intention_to_public_date
+                                )
+                                current_index += 1
+                                participant_level_data = cleaned_ps[current_index]
+                                current_index += 1
+                                basic_results = cleaned_ps[current_index]
+                                current_index += 1
+                                publication_list = cleaned_ps[current_index]
+                                current_index += 1
+                                publication_citations = cleaned_ps[current_index]
+                                current_index += 1
 
                                 data[url] = {
                                     "id": isrctn_id,
                                     "url": url,
                                     "timestamp": last_edited,
-                                    "title": title,
+                                    "title": title.text,
                                     "condition_category": condition_category,
                                     "date_applied": date_applied,
                                     "date_assigned": date_assigned,
@@ -112,7 +306,74 @@ def find(query):
                                     "overall_trial_status": overall_trial_status,
                                     "recruitment_status": recruitment_status,
                                     "summary": summary_data,
+                                    "primary_contact": primary_contact,
+                                    "additional_contacts": additional_contacts,
+                                    "numbers": {
+                                        "eudract_number": eudract_number,
+                                        "clinical_trials_gov_number": clinical_trials_gov_number,
+                                        "protocol_serial_number": protocol_serial_number,
+                                    },
+                                    "study_information": {
+                                        "scientific_title": scientific_title,
+                                        "acronym": acronym,
+                                        "study_hypothesis": study_hypothesis,
+                                        "ethics_approval": ethics_approval,
+                                        "study_design": study_design,
+                                        "primary_study_design": primary_study_design,
+                                        "secondary_study_design": secondary_study_design,
+                                        "trial_setting": trial_setting,
+                                        "trial_type": trial_type,
+                                        "patient_information_sheet": patient_information_sheet,
+                                        "condition": condition,
+                                        "intervention": intervention,
+                                        "intervention_type": intervention_type,
+                                        "phase": phase,
+                                        "drug_names": drug_names,
+                                        "primary_outcome_measure": primary_outcome_measure,
+                                        "secondary_outcome_measure": secondary_outcome_measure,
+                                        "overall_trial_start_date": overall_trial_start_date,
+                                        "overall_trial_end_date": overall_trial_end_date,
+                                        "reason_abandoned": reason_abandoned,
+                                    },
+                                    "elibibility": {
+                                        "participant_inclusion_criteria": participant_inclusion_criteria,
+                                        "participant_type": participant_type,
+                                        "age_group": age_group,
+                                        "gender": gender,
+                                        "target_num_participants": target_num_participants,
+                                        "participant_exclusion_criteria": participant_exclusion_criteria,
+                                        "recruitment_start_date": recruitment_start_date,
+                                        "recruitment_end_date": recruitment_end_date,
+                                    },
+                                    "locations": {
+                                        "countries_of_recruitment": countries_of_recruitment,
+                                        "trial_participation_centers": trial_participation_centers,
+                                    },
+                                    "sponsor": {
+                                        "organization": organization,
+                                        "sponsor_details": sponsor_details,
+                                        "sponsor_type": sponsor_type,
+                                        "sponsor_website": sponsor_website,
+                                    },
+                                    "funder": {
+                                        "funder_type": funder_type,
+                                        "funder_name": funder_name,
+                                        "alternative_name": alternative_name,
+                                        "funding_body_type": funding_body_type,
+                                        "funding_body_subtype": funding_body_subtype,
+                                        "location": location,
+                                    },
+                                    "results_and_publications": {
+                                        "publication_dissemination_plan": publication_dissemination_plan,
+                                        "intention_to_public_date": intention_to_public_date,
+                                        "participant_level_data": participant_level_data,
+                                        "basic_results": basic_results,
+                                        "publication_list": publication_list,
+                                        "publication_citations": publication_citations,
+                                    },
                                 }
+                                #for k, v in data[url].items():
+                                #    print(k, type(v))
                                 count += 1
 
     print(f"Fetched {count} results for {query}")
