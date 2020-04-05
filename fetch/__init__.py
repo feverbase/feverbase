49f895d8258219a1f1b50cd68b459c4d77089661
Gautam Mittal <gautam@mittal.net>
2020-04-04 23:26:23 -0700

    Fixed

--- fetch/__init__.py
+++ fetch/__init__.py
@@ -17,7 +17,7 @@ TERMS = utils.get_query_terms()
 
 def get_records():
     data = {}
-    for query in TERMS:
+    for query in TERMS[4:5]:
         # try:
         #     print(f"Crawling {chictr.SOURCE}...")
         #     data.update(chictr.find(query))
