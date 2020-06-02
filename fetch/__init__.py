492c12871ce06370255c75ce4ee5b63d08bb0c1d
Gautam Mittal <gautam@mittal.net>
2020-06-01 22:56:46 -0700

    Remove old_isrctn.py

--- fetch/__init__.py
+++ fetch/__init__.py
@@ -34,7 +34,6 @@ logger = logging.getLogger(__name__)
 
 TERMS = utils.get_query_terms()
 DRIPPING_FAUCETS = {
-    # chictr.SOURCE: chictr,
     clinicaltrialsgov.SOURCE: clinicaltrialsgov,
     eu.SOURCE: eu,
     isrctn.SOURCE: isrctn,
